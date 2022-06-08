df580fa03bee43805fef193043442c39ca56203e
Ashleen Rath <ashleen.rath@hotmail.com>
2022-06-08 16:09:28 +0100

    remove comments from old print stmts for testing

--- app.js
+++ app.js
@@ -8,20 +8,6 @@ let list = new LinkedList(new ListNode(10));
 list.addLast(20);
 list.addLast(30);
 
-// console.log(list.size());
-// console.log(list.getFirst());
-// console.log(list.getLast());
-
-// list.addFirst(5);
-// console.log(list.getFirst());
-
-//  Linked Lists are o(n) - respective of the size of the linked list
-//  To add/remove from the list is o(1) as its constant
-
-// console.log(list.indexOf(20));
-
-//console.log(list);
-
 list.reverse();
 
 list.printArray();
